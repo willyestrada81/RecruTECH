a956d12577b00bb93887efc1fc3e5cd797ae20d3
William Estrada <william.estrada@ninjarmm.com>
2020-04-23 19:59:00 -0400

    initial commit to heroku

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
     // DB Params
-    define("DB_HOST", "localhost");
-    define("DB_USER", "williamE");
-    define("DB_PASS", "password");
+    define("DB_HOST", "cleardb-transparent-47028");
+    define("DB_USER", "bef430aeb3aa43");
+    define("DB_PASS", "7fea6810");
     define("DB_NAME", "job_listing");
 
     define("SITE_TITLE", "JOBBIES");
