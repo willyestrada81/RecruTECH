416e919a03e46ce8138ff7889d22288f5fd79572
William Estrada <william.estrada@ninjarmm.com>
2020-04-23 20:09:07 -0400

    added db url

--- config/config.php
+++ config/config.php
@@ -1,6 +1,8 @@
 <?php
     // DB Params
-    define("DB_HOST", "cleardb-transparent-47028");
+
+    $url=parse_url(getenv(CLEARDB_DATABASE_URL));
+    define("DB_HOST", $url);
     define("DB_USER", "bef430aeb3aa43");
     define("DB_PASS", "7fea6810");
     define("DB_NAME", "job_listing");
