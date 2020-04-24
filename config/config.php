d923ad3f07d65a27570cbdac45ed0c4491424394
William Estrada <william.estrada@ninjarmm.com>
2020-04-23 20:37:52 -0400

    added db url

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
     // DB Params
 
-    $url=parse_url(getenv(CLEARDB_DATABASE_URL));
-    define("DB_HOST", $url);
+
+    define("DB_HOST", "us-cdbr-iron-east-01.cleardb.net");
     define("DB_USER", "bef430aeb3aa43");
     define("DB_PASS", "7fea6810");
     define("DB_NAME", "heroku_6dc20e74f6a81ea");
