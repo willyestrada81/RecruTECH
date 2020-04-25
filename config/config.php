7fd215c70749f678914e10910b17f748719eebdb
William Estrada <william.estrada@ninjarmm.com>
2020-04-25 12:41:47 -0400

    removed heroku env

--- config/config.php
+++ config/config.php
@@ -1,23 +1,9 @@
 <?php
     // DB Params
-
-    // include_once 'env.php';
-
-    $DB_HOST = getenv('DATABASE_URL');
-    $DB_USER = getenv('USER');
-    $DB_PASS = getenv('PASS');
-    $DB_NAME = getenv('DB');
-
-    define("DB_HOST", $DB_HOST);
-    define("DB_USER", $DB_USER);
-    define("DB_PASS", $DB_PASS);
-    define("DB_NAME", $DB_NAME);
-
-
-    // define("DB_HOST", "us-cdbr-iron-east-01.cleardb.net");
-    // define("DB_USER", "bef430aeb3aa43");
-    // define("DB_PASS", "7fea6810");
-    // define("DB_NAME", "heroku_6dc20e74f6a81ea");
+    define("DB_HOST", "us-cdbr-iron-east-01.cleardb.net");
+    define("DB_USER", "bef430aeb3aa43");
+    define("DB_PASS", "7fea6810");
+    define("DB_NAME", "heroku_6dc20e74f6a81ea");
 
     // define("DB_HOST", "localhost");
     // define("DB_USER", "williamE");
